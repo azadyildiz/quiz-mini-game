d0d1bf9a8319e6c064a8a60169513dc7ff351931
Azad YILDIZ <azad.yildiz119@gmail.com>
2021-07-20 02:38:17 +0300

    Back and next button working clearly. TODO: Selectable options

--- script.js
+++ script.js
@@ -31,10 +31,12 @@ function categoryChoose(event){
 }
 
 function createQuestions(category){
+    var divMain = document.createElement("div")
+    divMain.className = "questions"
     for(question of category.questions){
         var div = document.createElement("div")
         // WE JUST PRINT SCREEN FIRST QUESTION
-        if(category.questions.indexOf(question) == 0){
+        if(category.questions.indexOf(question) == 2){
             div.className = `${category.questions.indexOf(question)}`
         }
         else{
@@ -56,18 +58,54 @@ function createQuestions(category){
             divOption.appendChild(divOptions)
         }
         div.appendChild(divOption)
-        questionContainer.appendChild(div)
+        divMain.appendChild(div)
         //------------------------------------
     }
+    questionContainer.appendChild(divMain)
             // BACK AND NEXT BUTTON
             var divButton = document.createElement("div")
-            divButton.className = "button-container"
+            divButton.className = "buttons"
             var divButton1 = document.createElement("div")
             divButton1.className = "button"
             divButton1.textContent = "< Back"
+            divButton1.addEventListener("click",function(){
+                for(currentQuestion of divButton1.parentElement.parentElement.children[0].children){
+                    if(currentQuestion.classList.toString().includes("inv")){
+                    }
+                    else{
+                        var questionNumber = parseInt(currentQuestion.classList.toString())
+                        for(nextQuestion of currentQuestion.parentElement.children){
+                            if(nextQuestion.className.toString().includes(`${questionNumber-1}`)){
+                                nextQuestion.className = `${questionNumber-1}`
+                                currentQuestion.className = `${questionNumber} inv`
+                            }
+                        }
+                    }
+                }
+            })
             var divButton2 = document.createElement("div")
             divButton2.className = "button"
             divButton2.textContent = "Next >"
+            divButton2.addEventListener("click",function(){
+                var flag = false
+                for(currentQuestion of divButton2.parentElement.parentElement.children[0].children){
+                    if(currentQuestion.classList.toString().includes("inv")){
+                    }
+                    else{
+                        var questionNumber = parseInt(currentQuestion.classList.toString())
+                        for(nextQuestion of currentQuestion.parentElement.children){
+                            if(nextQuestion.className.toString().includes(`${questionNumber+1}`)){
+                                nextQuestion.className = `${questionNumber+1}`
+                                currentQuestion.className = `${questionNumber} inv`
+                                flag = true
+                            }
+                        }
+                    }
+                    if(flag){
+                        break
+                    }
+                }
+            })
             divButton.appendChild(divButton1)
             divButton.appendChild(divButton2)
             questionContainer.appendChild(divButton)

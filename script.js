589d3bf079191e2a579b9f7e0801b6d770d3a039
Azad YILDIZ <azad.yildiz119@gmail.com>
2021-07-24 14:32:16 +0300

    done. (I hope)

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ var categoryContainer = document.getElementById("categoryContainer")
 var questionContainer = document.getElementById("questionContainer")
 var popupContainer = document.getElementById("popupContainer")
 var resultContainer = document.getElementById("resultContainer")
-const alphabet = ["A","B","C","D","E"]
-// CATEGORY CHOOSE SCREEN
+
+const alphabet = ["A","B","C","D","E","F","G","H"]
+let categoryName
+
 for(category of categories){
     var div = document.createElement("div")
     div.className = "options"
@@ -13,25 +15,153 @@ for(category of categories){
     })
     categoryContainer.appendChild(div)
 }
-// WHEN CLICKED AN OPTION
-let categoryName
-function categoryChoose(event){
-    for(category of categories){
-        if(event.target.textContent == category.name){
-            categoryContainer.style.opacity = "0"
-            categoryContainer.style.zIndex = "0"
-            setTimeout(function(){
-                categoryContainer.className = "category-container container inv"
-                questionContainer.className = "question-container container"
-                setTimeout(function(){
-                    questionContainer.style.opacity = "1"
-                    questionContainer.style.zIndex = "2"
-                },500)
-            },500)
-            categoryName = category.name
-            createQuestions(category)
+
+popupContainer.addEventListener("click",function(e){
+    if(e.target.className == "button"){
+       if(e.target.textContent == "Yes"){
+           quizFinished()
+       }
+       else{
+           popupContainer.className = "popup-container inv"
+       }
+    }
+})
+
+function getBackQuestion(divButton1){
+    var flag = false
+    for(currentQuestion of divButton1.parentElement.parentElement.children[0].children){
+        if(currentQuestion.classList.toString().includes("inv")){
+        }
+        else{
+            var questionNumber = parseInt(currentQuestion.classList.toString())
+            for(nextQuestion of currentQuestion.parentElement.children){
+                if(nextQuestion.className.toString().includes(`${questionNumber-1}`)){
+                    nextQuestion.className = `${questionNumber-1}`
+                    currentQuestion.className = `${questionNumber} inv`
+                    flag = true
+                }
+            }
+        }
+        if(flag){
+            break
         }
     }
+}
+
+function getNextQuestion(divButton2){
+    var flag = false
+    for(currentQuestion of divButton2.parentElement.parentElement.children[0].children){
+        if(currentQuestion.classList.toString().includes("inv")){
+        }
+        else{
+            var questionNumber = parseInt(currentQuestion.classList.toString())
+            for(nextQuestion of currentQuestion.parentElement.children){
+                if(nextQuestion.className.toString().includes(`${questionNumber+1}`)){
+                    nextQuestion.className = `${questionNumber+1}`
+                    currentQuestion.className = `${questionNumber} inv`
+                    flag = true
+                }
+            }
+        }
+        if(flag){
+            break
+        }
+    }
+}
+
+function quizFinished(){
+    checkResult()
+    editPage()
+}
+
+function checkResult(){
+    var trueCounter = 0
+    var falseCounter = 0
+    var emptyCounter = 0
+    var flag = false
+    for(questionHTML of questionContainer.firstElementChild.children){
+        for(questionData of questions){
+            if(questionHTML.firstElementChild.textContent == questionData.question){
+                for(var i = 0; i<questionHTML.children[1].children.length;i++){
+                    if(questionHTML.children[1].children[i].className == "chosen"){
+                        if(i == questionData.answer){
+                            trueCounter++
+                            flag = true
+                        }
+                        else{
+                            falseCounter++
+                            flag = true
+                        }
+                    }
+                }
+                if(!flag){
+                    emptyCounter++
+                }
+                flag = false
+            }
+        }
+    }
+    spanHeader = document.createElement("span")
+    spanHeader.className = "result header"
+    spanHeader.textContent = "YOUR RESULT"
+
+    spanTrue = document.createElement("span")
+    spanTrue.className = "result"
+    spanTrue.textContent = `True Answer = ${trueCounter}`
+
+    spanFalse = document.createElement("span")
+    spanFalse.className = "result"
+    spanFalse.textContent = `False Answer = ${falseCounter}`
+
+    spanEmpty = document.createElement("span")
+    spanEmpty.className = "result"
+    spanEmpty.textContent = `Empty Answer = ${emptyCounter}`
+
+    deneme = document.createElement("a")
+
+    aAgain = document.createElement("a")
+    aAgain.setAttribute("href","index.html")
+    aAgain.className = "finish again"
+    aAgain.innerHTML = "Try Again"
+
+    resultContainer.appendChild(spanHeader)
+    resultContainer.appendChild(spanTrue)
+    resultContainer.appendChild(spanFalse)
+    resultContainer.appendChild(spanEmpty)
+    resultContainer.appendChild(aAgain)
+}
+
+function editPage(){
+    questionContainer.className = "question-container container inv"
+    popupContainer.className = "popup-container inv"
+    resultContainer.className = "result-container container"
+}
+
+function createChangeButton(){
+    var divButton = document.createElement("div")
+    divButton.className = "buttons"
+    var divButton1 = document.createElement("div")
+    divButton1.className = "button"
+    divButton1.textContent = "< Back"
+    divButton1.addEventListener("click",function(){
+        getBackQuestion(divButton1)
+    })
+    var divButton2 = document.createElement("div")
+    divButton2.className = "button"
+    divButton2.textContent = "Next >"
+    divButton2.addEventListener("click",function(){
+        getNextQuestion(divButton2)
+    })
+    divButton.appendChild(divButton1)
+    divButton.appendChild(divButton2)
+    questionContainer.appendChild(divButton)
+    var finish = document.createElement("div")
+    finish.className = "button finish"
+    finish.textContent = "Finish the quiz!"
+    finish.addEventListener("click",function(){
+        popupContainer.className = "popup-container"
+    })
+    questionContainer.appendChild(finish)
 }
 
 function createQuestions(category){
@@ -89,144 +219,25 @@ function createQuestions(category){
         //------------------------------------
     }
     questionContainer.appendChild(divMain)
-            // BACK AND NEXT BUTTON
-            var divButton = document.createElement("div")
-            divButton.className = "buttons"
-            var divButton1 = document.createElement("div")
-            divButton1.className = "button"
-            divButton1.textContent = "< Back"
-            divButton1.addEventListener("click",function(){
-                getBackQuestion(divButton1)
-            })
-            var divButton2 = document.createElement("div")
-            divButton2.className = "button"
-            divButton2.textContent = "Next >"
-            divButton2.addEventListener("click",function(){
-                getNextQuestion(divButton2)
-            })
-            divButton.appendChild(divButton1)
-            divButton.appendChild(divButton2)
-            questionContainer.appendChild(divButton)
-            var finish = document.createElement("div")
-            finish.className = "button finish"
-            finish.textContent = "Finish the quiz!"
-            finish.addEventListener("click",function(){
-                popupContainer.className = "popup-container"
-            })
-            questionContainer.appendChild(finish)
-            //********************
+            createChangeButton()
 
 }
-popupContainer.addEventListener("click",function(e){
-    if(e.target.className == "button"){
-       if(e.target.textContent == "Yes"){
-           quizFinished() // TODO
-       }
-       else{
-           popupContainer.className = "popup-container inv"
-       }
-    }
-})
 
-function getBackQuestion(divButton1){
-    var flag = false
-    for(currentQuestion of divButton1.parentElement.parentElement.children[0].children){
-        if(currentQuestion.classList.toString().includes("inv")){
-        }
-        else{
-            var questionNumber = parseInt(currentQuestion.classList.toString())
-            for(nextQuestion of currentQuestion.parentElement.children){
-                if(nextQuestion.className.toString().includes(`${questionNumber-1}`)){
-                    nextQuestion.className = `${questionNumber-1}`
-                    currentQuestion.className = `${questionNumber} inv`
-                    flag = true
-                }
-            }
-        }
-        if(flag){
-            break
+function categoryChoose(event){
+    for(category of categories){
+        if(event.target.textContent == category.name){
+            categoryContainer.style.opacity = "0"
+            categoryContainer.style.zIndex = "0"
+            setTimeout(function(){
+                categoryContainer.className = "category-container container inv"
+                questionContainer.className = "question-container container"
+                setTimeout(function(){
+                    questionContainer.style.opacity = "1"
+                    questionContainer.style.zIndex = "2"
+                },500)
+            },500)
+            categoryName = category.name
+            createQuestions(category)
         }
     }
-}
-function getNextQuestion(divButton2){
-    var flag = false
-    for(currentQuestion of divButton2.parentElement.parentElement.children[0].children){
-        if(currentQuestion.classList.toString().includes("inv")){
-        }
-        else{
-            var questionNumber = parseInt(currentQuestion.classList.toString())
-            for(nextQuestion of currentQuestion.parentElement.children){
-                if(nextQuestion.className.toString().includes(`${questionNumber+1}`)){
-                    nextQuestion.className = `${questionNumber+1}`
-                    currentQuestion.className = `${questionNumber} inv`
-                    flag = true
-                }
-            }
-        }
-        if(flag){
-            break
-        }
-    }
-}
-
-function quizFinished(){
-    var trueCounter = 0
-    var falseCounter = 0
-    var emptyCounter = 0
-    var flag = false
-    for(questionHTML of questionContainer.firstElementChild.children){
-        for(questionData of questions){
-            if(questionHTML.firstElementChild.textContent == questionData.question){
-                for(var i = 0; i<questionHTML.children[1].children.length;i++){
-                    if(questionHTML.children[1].children[i].className == "chosen"){
-                        if(i == questionData.answer){
-                            trueCounter++
-                            flag = true
-                        }
-                        else{
-                            falseCounter++
-                            flag = true
-                        }
-                    }
-                }
-                if(!flag){
-                    emptyCounter++
-                }
-                flag = false
-            }
-        }
-    }
-    // OTHER THINGS
-    questionContainer.className = "question-container container inv"
-    popupContainer.className = "popup-container inv"
-
-    spanHeader = document.createElement("span")
-    spanHeader.className = "result header"
-    spanHeader.textContent = "YOUR RESULT"
-
-    spanTrue = document.createElement("span")
-    spanTrue.className = "result"
-    spanTrue.textContent = `True Answer = ${trueCounter}`
-
-    spanFalse = document.createElement("span")
-    spanFalse.className = "result"
-    spanFalse.textContent = `False Answer = ${falseCounter}`
-
-    spanEmpty = document.createElement("span")
-    spanEmpty.className = "result"
-    spanEmpty.textContent = `Empty Answer = ${emptyCounter}`
-
-    deneme = document.createElement("a")
-
-    aAgain = document.createElement("a")
-    aAgain.setAttribute("href","index.html")
-    aAgain.className = "finish again"
-    aAgain.innerHTML = "Try Again"
-
-    resultContainer.appendChild(spanHeader)
-    resultContainer.appendChild(spanTrue)
-    resultContainer.appendChild(spanFalse)
-    resultContainer.appendChild(spanEmpty)
-    resultContainer.appendChild(aAgain)
-    resultContainer.className = "result-container container"
 }
